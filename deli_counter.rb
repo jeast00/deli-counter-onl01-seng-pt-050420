37b2c7384721a74bf2aeba3cf5e6c9766e1bac01
Jonathan East <jonathan_east@ymail.com>
2020-05-07 03:07:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,7 @@ def line(line_number)
   if line_number.length == 0
     puts "The line is currently empty."
   else
-    line_number.collect { |line, index|
-      puts "The line is currently: #{line.join(index+1)}. "
+
   }
   end
 end
