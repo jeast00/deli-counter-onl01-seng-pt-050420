8189e7a511f2b6e2d0af834764199df82027f986
Jonathan East <jonathan_east@ymail.com>
2020-05-07 04:03:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli_line)
     puts "The line is currently empty."
   else
     deli_line.each_with_index { |name, index|
-      index_number = index+1.join
+      index_number = index.join
     puts "The Line is currently: "
   end
 end
