ba8cd149fbe089663ec9eea992923ce0d67169e6
Jonathan East <jonathan_east@ymail.com>
2020-05-07 04:21:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,15 @@
 def line(deli_line)
   if !deli_line.length
     puts "The line is currently empty."
+  else
+
+    names_with_numbers = []
+
+    deli_line.each_with_index {|name, index|
+      names_with_numbers.push("#{index + 1}. #{name}")
+    }
+    puts "The line is currently: #{names_with_numbers.join(" ")}"
   end
-
-  names_with_numbers = []
-
-  deli_line.each_with_index {|name, index|
-    names_with_numbers.push("#{index + 1}. #{name}")
-  }
-
-  puts "The line is currently: #{names_with_numbers.join(" ")}"
 end
 
 def take_a_number(line_number, name)
